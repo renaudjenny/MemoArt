d9f9883fafcec7e0656517aa5e288e175d45a72b
Renaud Jenny <renaud.jenny@gmail.com>
2020-10-17 15:04:07 +0100

    build: create Memoiry iOS project

---
+++ Memoiry/MemoiryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoiryApp.swift
+//  Memoiry
+//
+//  Created by Renaud JENNY on 17/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemoiryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
