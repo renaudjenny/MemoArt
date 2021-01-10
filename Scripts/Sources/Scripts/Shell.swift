f430955f0a500dd2a615e27c7875042be2731fd7
Renaud Jenny <renaud.jenny@gmail.com>
2021-01-10 15:51:29 +0000

    build(CI): trying to extract the screenshot from xcresult
    * It seems to be very hard and not straightforward to extract
      screenshots from xcresult. Even with build tools that Apple provides.
      Hence, abandoning this for now

--- Scripts/Sources/Scripts/Shell.swift
+++ Scripts/Sources/Scripts/Shell.swift
@@ -1,9 +1,17 @@
 import Foundation
 
+let projectDirectoryPath = "~/Sources/MemoArt"
+enum ShellCommand: String {
+    case xcodebuild = "/usr/bin/xcodebuild"
+    case plutil = "/usr/bin/plutil"
+    case xmllint = "/usr/bin/xmllint"
+}
+
 // See https://stackoverflow.com/questions/26971240/how-do-i-run-a-terminal-command-in-a-swift-script-e-g-xcodebuild
 func shell(command: ShellCommand, arguments: [String] = []) -> (output: String?, status: Int32) {
     let task = Process()
     task.launchPath = command.rawValue
+    task.currentDirectoryPath = projectDirectoryPath
     task.arguments = arguments
 
     let pipe = Pipe()
@@ -14,9 +22,4 @@ func shell(command: ShellCommand, arguments: [String] = []) -> (output: String?,
     let output = String(data: data, encoding: .utf8)
     task.waitUntilExit()
     return (output, task.terminationStatus)
-}
-
-enum ShellCommand: String {
-    case ls = "/bin/ls"
-    case xcodebuild = "/usr/bin/xcodebuild"
 }
--- Scripts/Sources/Scripts/main.swift
+++ Scripts/Sources/Scripts/main.swift
@@ -1,5 +1,54 @@
+// Set the list of devices you want screenshot to be taken on
+let devicesName = [
+    "iPhone 12 Pro Max"
+]
+
+// Help you generate & find generated screenshots
+// See https://rderik.com/blog/understanding-xcuitest-screenshots-and-how-to-access-them/
+let derivedDataPath = "/tmp/DerivedDataMarketing"
+
 print("Run Marketing screenshots generation...")
+for deviceName in devicesName {
+    print("Currently running on Simulator named: \(deviceName)")
 
-let macOSTest = shell(command: .xcodebuild, arguments: ["clean", "test", "-scheme", "Memo Art (macOS)"])
+    let iOSMarketingTestPlan = shell(command: .xcodebuild, arguments: [
+        "test",
+        "-scheme", "MemoArt (iOS)",
+        "-destination", "platform=iOS Simulator,name=\(deviceName)",
+        "-derivedDataPath", derivedDataPath,
+        "-testPlan", "Marketing",
+    ])
 
-print(macOSTest.output ?? "Error")
+    // LATEST_XCRESULTS=`plutil -extract logs xml1 -o - ${DERIVED_DATA_PATH}/Logs/Test/LogStoreManifest.plist | xmllint --xpath 'string(//dict[1]/string[contains(text(),"xcresult")])' -`
+
+    let plutilExtractLogs = shell(command: .plutil, arguments: [
+        "-extract", "logs", "xml1",
+        "-o", "-", "\(derivedDataPath)/Logs/Test/LogStoreManifest.plist"
+    ])
+
+    guard
+        plutilExtractLogs.status == 0,
+        let plutilExtractLogsOutput = plutilExtractLogs.output
+    else {
+        print("plutil finished with errors")
+        print(plutilExtractLogs.output ?? "Cannot print errors...")
+        continue
+    }
+
+    let xmllint = shell(command: .xmllint, arguments: [
+        "--xpath",
+        "string(//dict[1]/string[contains(text(),\"xcresult\")])",
+        plutilExtractLogsOutput
+    ])
+
+    guard
+        xmllint.status == 0,
+        let xmllintOutput = xmllint.output
+    else {
+        print("xmllint finished with errors")
+        print(xmllint.output ?? "Cannot print errors...")
+        continue
+    }
+
+    print(xmllintOutput)
+}
